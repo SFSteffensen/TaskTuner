18466e7fed72771d5e78efa5f49da9e240d7953f
S3bTheGuy <sebastian1511@icloud.com>
2024-01-11 12:49:28 +0100

    Update greet function to include Tauri

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello, {}! You've been greeted from Rust! via Tauri", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
