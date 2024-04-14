2aed1f7aa9f7aa1b6d4a8e0903e46eff416bd4ad
S3bTheGuy <sebastian1511@icloud.com>
2024-04-14 18:36:15 +0200

    Update dependencies and add lazy_static and anyhow crates

--- src-tauri/src/scraper.rs
+++ src-tauri/src/scraper.rs
@@ -1,4 +1,5 @@
 use chrono::NaiveTime;
+use lazy_static::lazy_static;
 use regex::Regex;
 use reqwest::blocking::get;
 use reqwest::blocking::Client;
@@ -12,7 +13,33 @@ use serde::Serialize;
 use serde_json::json;
 use std::collections::HashMap;
 use std::error::Error;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+
+struct ClientManager {
+    client: Arc<Mutex<Option<Client>>>,
+}
+
+impl ClientManager {
+    fn new() -> Self {
+        ClientManager {
+            client: Arc::new(Mutex::new(None)),
+        }
+    }
+
+    fn set_client(&self, client: Client) {
+        let mut client_guard = self.client.lock().unwrap();
+        *client_guard = Some(client);
+    }
+
+    fn get_client(&self) -> Option<Client> {
+        let client_guard = self.client.lock().unwrap();
+        client_guard.clone()
+    }
+}
+
+lazy_static! {
+    static ref CLIENT_MANAGER: ClientManager = ClientManager::new();
+}
 
 #[derive(Serialize, Deserialize)]
 struct ClassDetails {
@@ -46,9 +73,9 @@ pub fn get_schools() -> HashMap<String, String> {
         .find(Name("div"))
         .flat_map(|n| n.find(Name("a")))
         .filter_map(|n| {
-            let schoolId = n.attr("href")?.split('/').nth(2)?.to_string();
+            let school_id = n.attr("href")?.split('/').nth(2)?.to_string();
             let school_name = n.text();
-            Some((schoolId, school_name))
+            Some((school_id, school_name))
         })
         .collect();
 
@@ -59,33 +86,25 @@ pub fn get_schools() -> HashMap<String, String> {
 pub fn login(school_id: &str, username: &str, password: &str) -> String {
     match attempt_login(school_id, username, password) {
         Ok(client) => {
-            let dashboard_info = scrape_dashboard(&client, school_id);
-            let schedule_info = scrape_schedule(&client, school_id); // Fetch the schedule
-
-            // Handle the results
-            match (dashboard_info, schedule_info) {
-                (Ok(dashboard), Ok(schedule)) => {
-                    // Serialize both dashboard and schedule as JSON
-                    json!({
-                        "status": "success",
-                        "dashboard": dashboard,
-                        "schedule": schedule // Include the schedule in the response
-                    }).to_string()
-                },
-                (Err(e), _) => json!({ "status": "error", "message": format!("Failed to scrape the dashboard: {}", e) }).to_string(),
-                (_, Err(e)) => json!({ "status": "error", "message": format!("Failed to scrape the schedule: {}", e) }).to_string(),
-            }
+            CLIENT_MANAGER.set_client(client);
+            json!({
+                "status": "success"
+            })
+            .to_string()
         }
         Err(e) => {
-            // Serialize the login error as JSON
             json!({ "status": "error", "message": format!("Failed to log in: {}", e) }).to_string()
         }
     }
 }
 
-fn attempt_login(schoolId: &str, username: &str, password: &str) -> Result<Client, Box<dyn Error>> {
+fn attempt_login(
+    school_id: &str,
+    username: &str,
+    password: &str,
+) -> Result<Client, Box<dyn Error>> {
     // Create a URL object from the login URL for cookie handling
-    let login_url = format!("https://www.lectio.dk/lectio/{}/login.aspx", schoolId);
+    let login_url = format!("https://www.lectio.dk/lectio/{}/login.aspx", school_id);
     let url = Url::parse(&login_url)?;
 
     // Create a cookie jar to hold the cookies
@@ -125,119 +144,142 @@ fn attempt_login(schoolId: &str, username: &str, password: &str) -> Result<Clien
     Ok(client)
 }
 
-fn scrape_dashboard(client: &Client, school_id: &str) -> Result<String, Box<dyn Error>> {
-    let dashboard_url = format!("https://www.lectio.dk/lectio/{}/forside.aspx", school_id);
-    let resp = client.get(&dashboard_url).send()?.text()?;
+fn scrape_dashboard(school_id: &str) -> Result<String, Box<dyn Error>> {
+    let client_opt = CLIENT_MANAGER.get_client();
+    if let Some(client) = client_opt {
+        let dashboard_url = format!("https://www.lectio.dk/lectio/{}/forside.aspx", school_id);
+        let resp = client.get(&dashboard_url).send()?;
+        let document = Html::parse_document(&resp.text()?);
 
-    let document = Html::parse_document(&resp);
-    let selector = Selector::parse("div#s_m_Content_Content_aktueltIsland_pa").unwrap();
+        let selector = Selector::parse("div#s_m_Content_Content_aktueltIsland_pa").unwrap();
 
-    let mut scraped_info = String::new();
+        let mut scraped_info = String::new();
 
-    if let Some(element) = document.select(&selector).next() {
-        for tr in element.select(&Selector::parse("tr").unwrap()) {
-            // Extracting text from each row
-            let text = tr.text().collect::<Vec<_>>().join(" ");
-            scraped_info.push_str(&format!("{}\n", text.trim()));
+        if let Some(element) = document.select(&selector).next() {
+            for tr in element.select(&Selector::parse("tr").unwrap()) {
+                // Extracting text from each row
+                let text = tr.text().collect::<Vec<_>>().join(" ");
+                scraped_info.push_str(&format!("{}\n", text.trim()));
+            }
+        } else {
+            return Err("Section not found".into());
         }
-    } else {
-        return Err("Section not found".into());
-    }
 
-    Ok(scraped_info)
+        Ok(scraped_info)
+    } else {
+        Err("Client is not initialized. Please login first.".into())
+    }
 }
 
-fn scrape_schedule(
-    client: &reqwest::blocking::Client,
-    school_id: &str,
-) -> Result<String, Box<dyn Error>> {
-    let schedule_url = format!("https://www.lectio.dk/lectio/{}/SkemaNy.aspx", school_id);
-    let resp = client.get(&schedule_url).send()?;
-    let document = Html::parse_document(&resp.text()?);
-
-    // Assuming each class block is within a 'div.s2skemabrikcontainer'
-    let class_selector = Selector::parse("div.s2skemabrikcontainer a.s2skemabrik").unwrap();
-
-    let status_regex = Regex::new(r"(Ændret!|Aflyst!)").unwrap();
-    let time_regex = Regex::new(r"(\d{2}:\d{2}) til (\d{2}:\d{2})").unwrap();
-    let room_regex = Regex::new(r"Lokale(?:r)?: ([^\n]+)").unwrap();
-    let description_regex = Regex::new(r"Øvrigt indhold:(.+?)(?:Note:|$)").unwrap();
-    let ressource_regex = Regex::new(r"Resource: (.+)").unwrap();
-    let teacher_regex = Regex::new(r"Lærer: ([^\n]+)").unwrap();
-    let note_regex = Regex::new(r"Note:(.+)").unwrap();
-
-    let mut classes = Vec::new();
-
-    for class_div in document.select(&class_selector) {
-        let tooltip = class_div.value().attr("data-tooltip").unwrap_or_default();
-        let detail_link = class_div.value().attr("href");
-
-        // Basic details from the tooltip
-        let status = status_regex
-            .captures(tooltip)
-            .map_or("normal".to_string(), |caps| caps[0].to_string());
-        let class_name = class_div
-            .select(&Selector::parse("span[data-lectiocontextcard]").unwrap())
-            .next()
-            .map_or_else(|| "".to_string(), |n| n.inner_html().trim().to_string());
-        let time = time_regex.captures(tooltip).map_or_else(
-            || "Time not found".to_string(),
-            |caps| format!("{} - {}", &caps[1], &caps[2]),
-        );
-        let description = description_regex
-            .captures(tooltip)
-            .and_then(|caps| caps.get(1))
-            .map_or(String::new(), |m| m.as_str().trim().to_string());
-        let ressource = ressource_regex
-            .captures(tooltip)
-            .map_or_else(|| "".to_string(), |caps| caps[1].trim().to_string());
-        let teacher = teacher_regex.captures(tooltip).map_or_else(
-            || "Teacher not found".to_string(),
-            |caps| caps[1].to_string(),
-        );
-        let room = room_regex
-            .captures(tooltip)
-            .map_or_else(|| "Room not found".to_string(), |caps| caps[1].to_string());
-        let notes = note_regex
-            .captures(tooltip)
-            .map_or_else(|| "".to_string(), |caps| caps[1].trim().to_string());
-
-        let mut detailed_homework = String::new();
-
-        if let Some(url) = detail_link {
-            let full_url = format!("https://www.lectio.dk{}", url);
-            println!("Fetching detailed page: {}", full_url);
-            let detail_response = client.get(full_url).send()?;
-            if detail_response.status().is_success() {
-                let detail_document = Html::parse_document(&detail_response.text()?);
-                let homework_selector =
-                    Selector::parse("div#s_m_Content_Content_tocAndToolbar_inlineHomeworkDiv")
-                        .unwrap();
-                detailed_homework = detail_document
-                    .select(&homework_selector)
-                    .next()
-                    .map(|div| div.text().collect::<Vec<_>>().join(" "))
-                    .unwrap_or_else(|| "No detailed homework provided".to_string());
-            } else {
-                println!(
-                    "Failed to fetch detailed page, status: {}",
-                    detail_response.status()
-                );
-            }
-        }
-
-        classes.push(ClassDetails {
-            status,
-            class_name,
-            teacher,
-            room,
-            description,
-            time,
-            homework: detailed_homework,
-            resources: ressource,
-            notes,
-        });
+#[tauri::command]
+pub fn get_dashboard(school_id: &str) -> String {
+    match scrape_dashboard(&school_id) {
+        Ok(dashboard) => dashboard,
+        Err(e) => format!("Error: {}", e),
     }
+}
 
-    serde_json::to_string(&classes).map_err(Into::into)
+fn scrape_schedule(school_id: &str) -> Result<String, Box<dyn Error>> {
+    let client_opt = CLIENT_MANAGER.get_client();
+
+    if let Some(client) = client_opt {
+        let schedule_url = format!("https://www.lectio.dk/lectio/{}/SkemaNy.aspx", school_id);
+        let resp = client.get(&schedule_url).send()?;
+        let document = Html::parse_document(&resp.text()?);
+
+        // Assuming each class block is within a 'div.s2skemabrikcontainer'
+        let class_selector = Selector::parse("div.s2skemabrikcontainer a.s2skemabrik").unwrap();
+
+        let status_regex = Regex::new(r"(Ændret!|Aflyst!)").unwrap();
+        let time_regex = Regex::new(r"(\d{2}:\d{2}) til (\d{2}:\d{2})").unwrap();
+        let room_regex = Regex::new(r"Lokale(?:r)?: ([^\n]+)").unwrap();
+        let description_regex = Regex::new(r"Øvrigt indhold:(.+?)(?:Note:|$)").unwrap();
+        let ressource_regex = Regex::new(r"Resource: (.+)").unwrap();
+        let teacher_regex = Regex::new(r"Lærer: ([^\n]+)").unwrap();
+        let note_regex = Regex::new(r"Note:(.+)").unwrap();
+
+        let mut classes = Vec::new();
+
+        for class_div in document.select(&class_selector) {
+            let tooltip = class_div.value().attr("data-tooltip").unwrap_or_default();
+            let detail_link = class_div.value().attr("href");
+
+            // Basic details from the tooltip
+            let status = status_regex
+                .captures(tooltip)
+                .map_or("normal".to_string(), |caps| caps[0].to_string());
+            let class_name = class_div
+                .select(&Selector::parse("span[data-lectiocontextcard]").unwrap())
+                .next()
+                .map_or_else(|| "".to_string(), |n| n.inner_html().trim().to_string());
+            let time = time_regex.captures(tooltip).map_or_else(
+                || "Time not found".to_string(),
+                |caps| format!("{} - {}", &caps[1], &caps[2]),
+            );
+            let description = description_regex
+                .captures(tooltip)
+                .and_then(|caps| caps.get(1))
+                .map_or(String::new(), |m| m.as_str().trim().to_string());
+            let ressource = ressource_regex
+                .captures(tooltip)
+                .map_or_else(|| "".to_string(), |caps| caps[1].trim().to_string());
+            let teacher = teacher_regex.captures(tooltip).map_or_else(
+                || "Teacher not found".to_string(),
+                |caps| caps[1].to_string(),
+            );
+            let room = room_regex
+                .captures(tooltip)
+                .map_or_else(|| "Room not found".to_string(), |caps| caps[1].to_string());
+            let notes = note_regex
+                .captures(tooltip)
+                .map_or_else(|| "".to_string(), |caps| caps[1].trim().to_string());
+
+            let mut detailed_homework = String::new();
+
+            if let Some(url) = detail_link {
+                let full_url = format!("https://www.lectio.dk{}", url);
+                println!("Fetching detailed page: {}", full_url);
+                let detail_response = client.get(full_url).send()?;
+                if detail_response.status().is_success() {
+                    let detail_document = Html::parse_document(&detail_response.text()?);
+                    let homework_selector =
+                        Selector::parse("div#s_m_Content_Content_tocAndToolbar_inlineHomeworkDiv")
+                            .unwrap();
+                    detailed_homework = detail_document
+                        .select(&homework_selector)
+                        .next()
+                        .map(|div| div.text().collect::<Vec<_>>().join(" "))
+                        .unwrap_or_else(|| "No detailed homework provided".to_string());
+                } else {
+                    println!(
+                        "Failed to fetch detailed page, status: {}",
+                        detail_response.status()
+                    );
+                }
+            }
+
+            classes.push(ClassDetails {
+                status,
+                class_name,
+                teacher,
+                room,
+                description,
+                time,
+                homework: detailed_homework,
+                resources: ressource,
+                notes,
+            });
+        }
+        serde_json::to_string(&classes).map_err(Into::into)
+    } else {
+        Err("Client is not initialized. Please login first.".into())
+    }
+}
+
+#[tauri::command]
+pub fn get_schedule(school_id: &str) -> String {
+    match scrape_schedule(&school_id) {
+        Ok(schedule) => schedule,
+        Err(e) => format!("Error: {}", e),
+    }
 }
--- src/Routes/DashBoard.tsx
+++ src/Routes/DashBoard.tsx
@@ -1,80 +1,46 @@
-import { createSignal, For, onMount } from 'solid-js';
+import { createSignal, onMount } from 'solid-js';
 import { useLocation } from "@solidjs/router";
 import { useStore } from "../store";
 import { invoke } from '@tauri-apps/api/core';
 
 function DashBoard() {
-    const {isLoggedIn} = useStore();
-    const {pathname} = useLocation();
+  const { isLoggedIn } = useStore();
+  const { pathname } = useLocation();
 
-    const [scheduleData, setScheduleData] = createSignal<any[]>([]);
+  const [dashboardData, setDashboardData] = createSignal('');
 
-    console.log(isLoggedIn())
+  if (!isLoggedIn()) {
+    console.log("User not logged in. Redirecting to login page.");
+    window.location.href = "/login?redirect=" + pathname;
+  }
 
-    if (!isLoggedIn()) {
-        console.log("User not logged in. Redirecting to login page.");
-        window.location.href = "/login?redirect=" + pathname;
+  async function fetchDashboard() {
+    try {
+      const schoolId = localStorage.getItem('selectedSchoolId') || '';
+      console.log("School ID being sent to get_dashboard:", schoolId);
+      const response = await invoke('get_dashboard', { schoolId: schoolId });
+      console.log("Dashboard response:", response);
+      setDashboardData(response);
+    } catch (error) {
+      console.error("Error fetching dashboard:", error);
     }
+  }
 
-    async function fetchSchedule() {
-        try {
-            const response = await invoke('get_schedule');
-            const responseData = JSON.parse(response); // assuming response is a JSON string
-
-            if (responseData.status === "success") {
-                // Assuming responseData.schedule holds the schedule JSON string
-                const scheduleJson = JSON.parse(responseData.schedule); // parse the JSON string into an object
-                setScheduleData(scheduleJson); // update the state
-
-                console.log("Schedule Data:", scheduleJson); // logging the parsed JSON
-            } else {
-                // Handle error case
-                console.error(responseData.message || "Failed to fetch schedule.");
-            }
-        } catch (error) {
-            console.error("Fetch schedule error:", error);
-        }
+  onMount(() => {
+    if (isLoggedIn()) {
+      fetchDashboard();
     }
+  });
 
-    onMount(fetchSchedule);
-
-    return (
-        <div>
-            <h1>Dashboard</h1>
-
-            <div>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Class Name</th>
-                        <th>Teacher</th>
-                        <th>Room</th>
-                        <th>Description</th>
-                        <th>Time</th>
-                        <th>Homework</th>
-                        {/* New column for homework */}
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {/*<For each={scheduleData()}>
-                        {(classDetail) => (
-                            <tr>
-                                <td>{classDetail.class_name}</td>
-                                <td>{classDetail.teacher}</td>
-                                <td>{classDetail.room}</td>
-                                <td>{classDetail.description}</td>
-                                <td>{classDetail.time}</td>
-                                <td>{classDetail.homework}</td>
-                                 Displaying homework data
-                            </tr>
-                        )}
-                    </For>*/}
-                    </tbody>
-                </table>
-            </div>
-
-        </div>
-    );
+  return (
+    <div class="p-4">
+      <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
+      <div class="bg-white p-4 rounded shadow">
+        <h2 class="text-xl">Dashboard Information:</h2>
+        <p class="whitespace-pre-wrap">{dashboardData()}</p>
+      </div>
+    </div>
+  );
 }
 
 export default DashBoard;
