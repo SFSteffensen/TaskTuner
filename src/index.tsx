7a0943107254c3bb954e5433f7b1cdd3e1555643
S3bTheGuy <sebastian1511@icloud.com>
2024-04-14 21:42:51 +0200

    Refactor scraper.rs to include day field and update schedule table in Schedule.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import { createSignal } from "solid-js";
 import { render } from "solid-js/web";
 import { Router, Route } from "@solidjs/router";
 
@@ -6,12 +5,13 @@ import "./styles.css";
 import Login from "./Routes/Login.tsx";
 import DashBoard from "./Routes/DashBoard.tsx";
 import NotFound from "./Routes/404.tsx";
-
+import Schedule from "./Routes/Schedule.tsx";
 
 render(() => (
   <Router>
-    <Route path={"/"} component={() => <DashBoard  />} />
-    <Route path={"/login"} component={() => <Login  />} />
+    <Route path={"/"} component={() => <DashBoard />} />
+    <Route path={"/schedule"} component={() => <Schedule />} />
+    <Route path={"/login"} component={() => <Login />} />
     <Route path={"*404"} component={NotFound} />
   </Router>
 ), document.getElementById("root") as HTMLElement);
