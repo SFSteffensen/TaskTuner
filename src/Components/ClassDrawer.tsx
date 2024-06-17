b539ecc819ffff3fb039881ee2cbdf557df71e00
S3bTheGuy <sebastian1511@icloud.com>
2024-06-17 12:51:21 +0200

    MouseDown FTW

--- src/Components/ClassDrawer.tsx
+++ src/Components/ClassDrawer.tsx
@@ -50,7 +50,7 @@ function ClassDrawer(props: ClassModalProps) {
         <div class="p-6">
           <button
             class="btn btn-primary btn-sm m-2"
-            onClick={toggleModal}>
+            onMouseDown={toggleModal}>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
               stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
--- src/Components/ScheduleCard.tsx
+++ src/Components/ScheduleCard.tsx
@@ -3,8 +3,8 @@ import ClassDrawer from "./ClassDrawer.tsx";
 
 
 interface ScheduleCardProps {
-    scheduled_class?: Class;
-    status?: string;
+  scheduled_class?: Class;
+  status?: string;
 }
 
 const BASE_CARD_CLASS = 'card bg-base-100 shadow-xl';
@@ -12,50 +12,50 @@ const CANCELLED_CARD_CLASS = 'card bg-error shadow-xl opacity-50';
 const CHANGED_CARD_CLASS = 'card bg-info shadow-xl';
 
 
-export default function ScheduleCard({scheduled_class}: ScheduleCardProps): JSX.Element {
-    const [isModalOpen, setModalOpen] = createSignal(false);
+export default function ScheduleCard({ scheduled_class }: ScheduleCardProps): JSX.Element {
+  const [isModalOpen, setModalOpen] = createSignal(false);
 
-    // Use a more descriptive function name
-    function getCardClassBasedOnStatus(status: string) {
-        let cardClass = BASE_CARD_CLASS;
+  // Use a more descriptive function name
+  function getCardClassBasedOnStatus(status: string) {
+    let cardClass = BASE_CARD_CLASS;
 
-        if (status === 'Aflyst!') {
-            cardClass = CANCELLED_CARD_CLASS;
-        } else if (status === 'Ændret!') {
-            cardClass = CHANGED_CARD_CLASS;
-        }
-        return cardClass;
+    if (status === 'Aflyst!') {
+      cardClass = CANCELLED_CARD_CLASS;
+    } else if (status === 'Ændret!') {
+      cardClass = CHANGED_CARD_CLASS;
     }
+    return cardClass;
+  }
 
-    function openModal() {
-        setModalOpen(true);
-    }
+  function openModal() {
+    setModalOpen(true);
+  }
 
-    if (!scheduled_class) {
-        return (
-            <div class="card opacity-50 bg-base-300 shadow-xl">
-                <div class="card-body p-2 text-center">
-                    <h2 class="text-sm font-bold">Ingen Lektion</h2>
-                    <p class="text-xs">Nyd Pausen</p>
-                </div>
-            </div>
-        );
-    }
-
+  if (!scheduled_class) {
     return (
-        <>
-            <div class={`${getCardClassBasedOnStatus(scheduled_class.status)} hover:scale-105 transition-transform`}
-                 onClick={openModal}>
-                <div class="card-body p-2 ">
-                    <h2 class="card-title text-sm font-bold">
-                        {scheduled_class.class_name}
-                    </h2>
-                    <p class="text-xs">{scheduled_class.teacher}</p>
-                    <p class="text-xs">{scheduled_class.room}</p>
-                </div>
-            </div>
-            {isModalOpen() &&
-                <ClassDrawer initialShow={isModalOpen()} class={scheduled_class} setModalOpen={setModalOpen}/>}
-        </>
+      <div class="card opacity-50 bg-base-300 shadow-xl">
+        <div class="card-body p-2 text-center">
+          <h2 class="text-sm font-bold">Ingen Lektion</h2>
+          <p class="text-xs">Nyd Pausen</p>
+        </div>
+      </div>
     );
+  }
+
+  return (
+    <>
+      <div class={`${getCardClassBasedOnStatus(scheduled_class.status)} hover:scale-105 transition-transform`}
+        onMouseDown={openModal}>
+        <div class="card-body p-2 ">
+          <h2 class="card-title text-sm font-bold">
+            {scheduled_class.class_name}
+          </h2>
+          <p class="text-xs">{scheduled_class.teacher}</p>
+          <p class="text-xs">{scheduled_class.room}</p>
+        </div>
+      </div>
+      {isModalOpen() &&
+        <ClassDrawer initialShow={isModalOpen()} class={scheduled_class} setModalOpen={setModalOpen} />}
+    </>
+  );
 }
