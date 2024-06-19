594233b2890a7f49c7c787f1c44a5ab99442dacd
S3bTheGuy <sebastian1511@icloud.com>
2024-06-19 14:50:41 +0200

    moved and translated Calendar button, and added calendar to individual classes

--- src/Components/ClassDrawer.tsx
+++ src/Components/ClassDrawer.tsx
@@ -1,155 +1,192 @@
 import { createEffect, createSignal, onCleanup } from 'solid-js';
 import { open } from '@tauri-apps/plugin-shell';
+import { save } from '@tauri-apps/plugin-dialog';
+import { writeFile } from '@tauri-apps/plugin-fs';
+import { createICalendarEvents } from '../Util/createCalendarEvents';
 
 interface ClassModalProps {
-    initialShow: boolean;
-    class?: Class;
-    setModalOpen: (value: boolean) => void;
+  initialShow: boolean;
+  class?: Class;
+  setModalOpen: (value: boolean) => void;
 }
 
 const TRANSLATE_X_FULL = 'translate-x-full';
 const TRANSLATE_X_ZERO = 'translate-x-0';
 
 const dayMap = {
-    ma: 'Mandag',
-    ti: 'Tirsdag',
-    on: 'Onsdag',
-    to: 'Torsdag',
-    fr: 'Fredag',
+  ma: 'Mandag',
+  ti: 'Tirsdag',
+  on: 'Onsdag',
+  to: 'Torsdag',
+  fr: 'Fredag',
 };
 
 function ClassDrawer(props: ClassModalProps) {
-    let drawerRef: HTMLDivElement | undefined;
-    const [isModalOpen, setModalOpen] = createSignal(props.initialShow);
-    const [translateX, setTranslateX] = createSignal(
-        isModalOpen() ? TRANSLATE_X_ZERO : TRANSLATE_X_FULL
-    );
+  let drawerRef: HTMLDivElement | undefined;
+  const [isModalOpen, setModalOpen] = createSignal(props.initialShow);
+  const [translateX, setTranslateX] = createSignal(
+    isModalOpen() ? TRANSLATE_X_ZERO : TRANSLATE_X_FULL
+  );
 
-    createEffect(() => {
-        if (isModalOpen()) {
-            setTranslateX(TRANSLATE_X_FULL);
-            setTimeout(() => setTranslateX(TRANSLATE_X_ZERO), 0);
-        } else {
-            setTranslateX(TRANSLATE_X_FULL);
-            setTimeout(() => setTranslateX(TRANSLATE_X_ZERO), 300); // Adjust this to match the duration of your CSS transition
-        }
+  createEffect(() => {
+    if (isModalOpen()) {
+      setTranslateX(TRANSLATE_X_FULL);
+      setTimeout(() => setTranslateX(TRANSLATE_X_ZERO), 0);
+    } else {
+      setTranslateX(TRANSLATE_X_FULL);
+      setTimeout(() => setTranslateX(TRANSLATE_X_ZERO), 300); // Adjust this to match the duration of your CSS transition
+    }
+  });
+
+  const toggleModal = () => {
+    setModalOpen(false);
+    setTimeout(() => props.setModalOpen(false), 300);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (drawerRef && !drawerRef.contains(event.target as Node)) {
+      toggleModal();
+    }
+  };
+
+  createEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    onCleanup(() => {
+      document.removeEventListener('mousedown', handleClickOutside);
     });
+  });
 
-    const toggleModal = () => {
-        setModalOpen(false);
-        setTimeout(() => props.setModalOpen(false), 300);
-    };
+  const getFullDayName = (shortDay: string) => {
+    return dayMap[shortDay] || shortDay;
+  };
 
-    const handleClickOutside = (event: MouseEvent) => {
-        if (drawerRef && !drawerRef.contains(event.target as Node)) {
-            toggleModal();
+  const openInBrowser = async (url: string) => {
+    try {
+      await open(url);
+    } catch (e) {
+      console.error('Failed to open URL', e);
+    }
+  };
+
+  const downloadClassSchedule = async () => {
+    if (!props.class) return;
+    const iCalendarEvents = createICalendarEvents([props.class]);
+
+    await save({
+      defaultPath: `${props.class.class_name}.ics`,
+      filters: [
+        {
+          name: 'iCalendar',
+          extensions: ['ics'],
+        },
+      ],
+    })
+      .then((res) => {
+        if (res === null) {
+          message('Ingen Undervisningsplan blev downloadet');
+          return;
         }
-    };
 
-    createEffect(() => {
-        document.addEventListener('mousedown', handleClickOutside);
-        onCleanup(() => {
-            document.removeEventListener('mousedown', handleClickOutside);
+        writeFile(res, new TextEncoder().encode(iCalendarEvents), {
+          create: true,
         });
-    });
 
-    const getFullDayName = (shortDay: string) => {
-        return dayMap[shortDay] || shortDay;
-    };
+        message('Skema downloadet med succes');
+      })
+      .catch((err) => {
+        message('Der opstod en fejl under download af tidsplanen');
+        console.error(err);
+      });
+  };
 
-    const openInBrowser = async (url: string) => {
-        try {
-            await open(url);
-        } catch (e) {
-            console.error('Failed to open URL', e);
-        }
-    };
-
-    return (
-        <div class={`fixed z-10 inset-0 overflow-y-auto flex justify-end`}>
-            <div
-                ref={drawerRef}
-                class={`card bg-white shadow-lg transform transition-transform duration-500 ${translateX()} max-w-md w-full`}
+  return (
+    <div class={`fixed z-10 inset-0 overflow-y-auto flex justify-end`}>
+      <div
+        ref={drawerRef}
+        class={`card bg-base-200 shadow-lg transform transition-transform duration-500 ${translateX()} max-w-md w-full`}
+      >
+        <div class="p-6">
+          <div class="flex justify-between items-center">
+            <h2 class="text-lg leading-6 font-medium">
+              {props.class?.class_name}
+            </h2>
+            <button
+              class="btn btn-primary btn-sm"
+              onMouseDown={toggleModal}
             >
-                <div class="p-6">
-                    <div class="flex justify-between items-center">
-                        <h2 class="text-lg leading-6 font-medium">
-                            {props.class?.class_name}
-                        </h2>
-                        <button
-                            class="btn btn-primary btn-sm"
-                            onMouseDown={toggleModal}
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-6 w-6"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                    <a
-                        onMouseDown={() =>
-                            openInBrowser(props.class?.detailed_link)
-                        }
-                        class="cursor-pointer opacity-50 text-secondary"
-                    >
-                        Se På Lectio
-                    </a>
-                    <div class="mt-4 space-y-2">
-                        <p>
-                            <span class="font-bold">Status:</span>{' '}
-                            {props.class?.status}
-                        </p>
-                        <p>
-                            <span class="font-bold">Klassenavn:</span>{' '}
-                            {props.class?.class_name}
-                        </p>
-                        <p>
-                            <span class="font-bold">Lærer:</span>{' '}
-                            {props.class?.teacher}
-                        </p>
-                        <p>
-                            <span class="font-bold">Lokale:</span>{' '}
-                            {props.class?.room}
-                        </p>
-                        <p>
-                            <span class="font-bold">Beskrivelse:</span>{' '}
-                            {props.class?.description}
-                        </p>
-                        <p>
-                            <span class="font-bold">Tid:</span>{' '}
-                            {props.class?.time}
-                        </p>
-                        <p>
-                            <span class="font-bold">Dag:</span>{' '}
-                            {getFullDayName(props.class?.day)}
-                        </p>
-                        <p>
-                            <span class="font-bold">Lektier:</span>{' '}
-                            {props.class?.homework}
-                        </p>
-                        <p>
-                            <span class="font-bold">Ressourcer:</span>{' '}
-                            {props.class?.resources}
-                        </p>
-                        <p>
-                            <span class="font-bold">Noter:</span>{' '}
-                            {props.class?.notes}
-                        </p>
-                    </div>
-                </div>
-            </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <a
+            onMouseDown={() =>
+              openInBrowser(props.class?.detailed_link)
+            }
+            class="cursor-pointer opacity-50 text-secondary"
+          >
+            Se På Lectio
+          </a>
+          <div class="mt-4 space-y-2">
+            <p>
+              <span class="font-bold">Status:</span>{' '}
+              {props.class?.status}
+            </p>
+            <p>
+              <span class="font-bold">Klassenavn:</span>{' '}
+              {props.class?.class_name}
+            </p>
+            <p>
+              <span class="font-bold">Lærer:</span>{' '}
+              {props.class?.teacher}
+            </p>
+            <p>
+              <span class="font-bold">Lokale:</span>{' '}
+              {props.class?.room}
+            </p>
+            <p>
+              <span class="font-bold">Beskrivelse:</span>{' '}
+              {props.class?.description}
+            </p>
+            <p>
+              <span class="font-bold">Tid:</span>{' '}
+              {props.class?.time}
+            </p>
+            <p>
+              <span class="font-bold">Dag:</span>{' '}
+              {getFullDayName(props.class?.day)}
+            </p>
+            <p>
+              <span class="font-bold">Lektier:</span>{' '}
+              {props.class?.homework}
+            </p>
+            <p>
+              <span class="font-bold">Ressourcer:</span>{' '}
+              {props.class?.resources}
+            </p>
+            <p>
+              <span class="font-bold">Noter:</span>{' '}
+              {props.class?.notes}
+            </p>
+            <button class="btn btn-primary" onMouseDown={downloadClassSchedule}>
+              Tilføj Lektion til Kalender
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default ClassDrawer;
--- src/Components/DesktopSchedule.tsx
+++ src/Components/DesktopSchedule.tsx
@@ -7,111 +7,113 @@ import { message, save } from '@tauri-apps/plugin-dialog';
 import { writeFile } from '@tauri-apps/plugin-fs';
 
 function DesktopSchedule() {
-    const {
-        scheduleData,
-        selectedWeek,
-        handleWeekChange,
-        getCachedScheduleData,
-    } = useScheduleData();
+  const {
+    scheduleData,
+    selectedWeek,
+    handleWeekChange,
+    getCachedScheduleData,
+  } = useScheduleData();
 
-    async function downloadSchedule() {
-        // Get the current week's schedule data
-        const currentWeekSchedule = getCachedScheduleData(selectedWeek());
+  async function downloadSchedule() {
+    // Get the current week's schedule data
+    const currentWeekSchedule = getCachedScheduleData(selectedWeek());
 
-        const iCalendarEvents = createICalendarEvents(currentWeekSchedule);
+    const iCalendarEvents = createICalendarEvents(currentWeekSchedule);
 
-        await save({
-            defaultPath: 'schedule.ics',
-            filters: [
-                {
-                    name: 'iCalendar',
-                    extensions: ['ics'],
-                },
-            ],
-        })
-            .then((res) => {
-                // assert that the user didn't cancel the dialog
-                if (res === null) {
-                    message('This week has no classes!');
-                    return;
-                }
+    await save({
+      defaultPath: 'Skema.ics',
+      filters: [
+        {
+          name: 'iCalendar',
+          extensions: ['ics'],
+        },
+      ],
+    })
+      .then((res) => {
+        // assert that the user didn't cancel the dialog
+        if (res === null) {
+          message('Ingen Undervisningsplan blev downloadet');
+          return;
+        }
 
-                writeFile(res, new TextEncoder().encode(iCalendarEvents), {
-                    create: true,
-                });
+        writeFile(res, new TextEncoder().encode(iCalendarEvents), {
+          create: true,
+        });
 
-                message('Schedule downloaded successfully');
-            })
-            .catch((err) => {
-                message('An error occurred while downloading the schedule');
-                console.error(err);
-            });
-    }
+        message('Skema downloadet med succes');
+      })
+      .catch((err) => {
+        message('Der opstod en fejl under download af tidsplanen');
+        console.error(err);
+      });
+  }
 
-    return (
-        <div class="overflow-x-auto p-4">
-            <h1 class="text-2xl font-bold mb-4">Class Schedule</h1>
-            <div class="mb-4 flex justify-center">
-                <button class="btn btn-primary" onClick={downloadSchedule}>
-                    Download Schedule
-                </button>
-                <ul class="menu menu-horizontal bg-base-200 rounded-box hidden lg:flex">
-                    {Array.from(
-                        { length: 13 },
-                        (_, i) => selectedWeek() - 6 + i
-                    ).map((week) => (
-                        <li
-                            key={week}
-                            class={
-                                week === selectedWeek()
-                                    ? 'active bg-base-300'
-                                    : week === getWeekNumber(new Date())
-                                      ? 'bg-accent'
-                                      : ''
-                            }
-                            onMouseDown={() => handleWeekChange(week)}
-                        >
-                            <a>Uge {week}</a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div class="w-full">
-                <table class="table w-full table-compact table-fixed hidden lg:table">
-                    <thead>
-                        <tr>
-                            <th class="w-16">Tid</th>
-                            {DAYS.map((day) => (
-                                <th key={day}>
-                                    {DAY_NAMES[DAYS.indexOf(day)]}
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {Object.keys(scheduleData()).map((time) => (
-                            <tr key={time}>
-                                <td>{time}</td>
-                                {DAYS.map((day) => (
-                                    <td key={day}>
-                                        {scheduleData()[time][day] ? (
-                                            <ScheduleCard
-                                                scheduled_class={
-                                                    scheduleData()[time][day]
-                                                }
-                                            />
-                                        ) : (
-                                            <ScheduleCard />
-                                        )}
-                                    </td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div class="overflow-x-auto p-4">
+      <div class="flex justify-between items-center mb-4">
+        <h1 class="text-2xl font-bold">Class Schedule</h1>
+        <button class="btn btn-primary" onMouseDown={downloadSchedule}>
+          Tilføj Uge til Kalender
+        </button>
+      </div>
+      <div class="mb-4 flex justify-center">
+        <ul class="menu menu-horizontal bg-base-200 rounded-box hidden lg:flex">
+          {Array.from(
+            { length: 13 },
+            (_, i) => selectedWeek() - 6 + i
+          ).map((week) => (
+            <li
+              key={week}
+              class={
+                week === selectedWeek()
+                  ? 'active bg-base-300'
+                  : week === getWeekNumber(new Date())
+                    ? 'bg-accent'
+                    : ''
+              }
+              onMouseDown={() => handleWeekChange(week)}
+            >
+              <a>Uge {week}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div class="w-full">
+        <table class="table w-full table-compact table-fixed hidden lg:table">
+          <thead>
+            <tr>
+              <th class="w-16">Tid</th>
+              {DAYS.map((day) => (
+                <th key={day}>
+                  {DAY_NAMES[DAYS.indexOf(day)]}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(scheduleData()).map((time) => (
+              <tr key={time}>
+                <td>{time}</td>
+                {DAYS.map((day) => (
+                  <td key={day}>
+                    {scheduleData()[time][day] ? (
+                      <ScheduleCard
+                        scheduled_class={
+                          scheduleData()[time][day]
+                        }
+                      />
+                    ) : (
+                      <ScheduleCard />
+                    )}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default DesktopSchedule;
